546d236486618786a8ac4cab68a035af0b9f7c07
fabiodagostino27 <fabiodago27@gmail.com>
2025-06-17 16:58:08 +0200

    Refactor VideogameService to include CRUD operations and enhance repository interactions

--- src/main/java/org/final_project/java/progetto_finale_java/repo/VideogameRepository.java
+++ src/main/java/org/final_project/java/progetto_finale_java/repo/VideogameRepository.java
@@ -1,8 +1,10 @@
 package org.final_project.java.progetto_finale_java.repo;
 
+import java.util.List;
+
 import org.final_project.java.progetto_finale_java.model.Videogame;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface VideogameRepository extends JpaRepository<Videogame, Integer>{
-    
+    public List<Videogame> findByTitleContainingIgnoreCase(String title);
 }
--- src/main/java/org/final_project/java/progetto_finale_java/service/VideogameService.java
+++ src/main/java/org/final_project/java/progetto_finale_java/service/VideogameService.java
@@ -1,5 +1,9 @@
 package org.final_project.java.progetto_finale_java.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.final_project.java.progetto_finale_java.model.Videogame;
 import org.final_project.java.progetto_finale_java.repo.VideogameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -8,4 +12,32 @@ import org.springframework.stereotype.Service;
 public class VideogameService {
     @Autowired
     private VideogameRepository repository;
+
+    public List<Videogame> getAll() {
+        return repository.findAll();
+    }
+
+    public Optional<Videogame> getById(Integer id) {
+        return repository.findById(id);
+    }
+
+    public List<Videogame> getByTitle(String title) {
+        return repository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public Videogame create(Videogame videogame) {
+        return repository.save(videogame);
+    }
+
+    public Videogame update(Videogame videogame) {
+        return repository.save(videogame);
+    }
+
+    public void delete(Videogame videogame) {
+        repository.delete(videogame);
+    }
+
+    public void deleteById(Integer id) {
+        repository.deleteById(id);
+    }
 }
