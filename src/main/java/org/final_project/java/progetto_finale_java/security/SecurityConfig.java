4587ac731f2f9fb74f9ab72bfe1079e78166a761
fabiodagostino27 <fabiodago27@gmail.com>
2025-06-19 17:21:31 +0200

    Refactor SecurityConfig to enhance user authentication setup; add DatabaseUserDetailsService and PasswordEncoder beans.

--- src/main/java/org/final_project/java/progetto_finale_java/security/SecurityConfig.java
+++ src/main/java/org/final_project/java/progetto_finale_java/security/SecurityConfig.java
@@ -2,16 +2,50 @@ package org.final_project.java.progetto_finale_java.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
     @Bean
+    @SuppressWarnings("removal")
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
+        http.authorizeHttpRequests()
+            .requestMatchers("/videogames/create", "videogames/edit/**").hasAuthority("ADMIN")
+            .requestMatchers(HttpMethod.POST, "/videogames/**").hasAuthority("ADMIN")
+            .requestMatchers("/platforms/create", "platforms/edit/**").hasAuthority("ADMIN")
+            .requestMatchers(HttpMethod.POST, "/platforms/**").hasAuthority("ADMIN")
+            .requestMatchers("/**").hasAnyAuthority("USER", "ADMIN")
+            .and().formLogin()
+            .and().logout()
+            .and().exceptionHandling();
+            
         return http.build();
+    }
+
+    @Bean
+    DatabaseUserDetailsService userDetailsService() {
+        return new DatabaseUserDetailsService();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    @SuppressWarnings("deprecation")
+    AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 }
