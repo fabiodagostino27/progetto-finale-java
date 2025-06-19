159795167497feb9ee872fe0e725087314a70797
fabiodagostino27 <fabiodago27@gmail.com>
2025-06-19 16:52:51 +0200

    Add DatabaseUserDetails and SecurityConfig classes for user authentication

---
+++ src/main/java/org/final_project/java/progetto_finale_java/security/SecurityConfig.java
@@ -0,0 +1,10 @@
+package org.final_project.java.progetto_finale_java.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    
+}
